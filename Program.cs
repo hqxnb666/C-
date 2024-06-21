058f005cdceacaeaa3f20fecde2621266ec1396e
hqx <130121049+hqxnb666@users.noreply.github.com>
2024-06-21 21:44:48 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<StudentContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("StudentMSDatabase"), new MySqlServerVersion(new Version(8, 0, 23))));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Students}/{action=Index}/{id?}");
+
+app.Run();
