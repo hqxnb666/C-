db5cb9a765097e2b8a8f64cdfd4eef2a37c88a36
hqx <130121049+hqxnb666@users.noreply.github.com>
2024-08-13 23:49:49 +0800

    Add files via upload

---
+++ test2.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+//³éÏóÀà
+class Car
+{
+public:
+	//´¿Ğéº¯Êı
+	virtual void Drive() = 0;
+};
+int main()
+{
+	Car c;
+	
+	return 0;
+}
