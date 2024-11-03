5b07ad5d6684250407c60173c3c479d7a54cd06d
hqx <130121049+hqxnb666@users.noreply.github.com>
2024-11-03 23:59:34 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,17 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-//#include <iostream>
-#include "AVLTree.h"
-//using namespace std;
+#include <iostream>
+using namespace std;
+class Person {
+public:
+	 void BuyTIcket() { 
+		cout << "买票全价" << endl; }
+};
+
+class Student : public Person {
+public:
+	virtual void BuyTIcket() {
+		cout << "买票半价" << endl;
+	}
+};
+//多态的条件
+//虚函数的重写（父子类虚函数，要求三同  -- 函数名 参数 返回值）
+//                 1.例外（协变）
+//                 2.（析构函数）建议析构函数写成虚函数，防止内存泄漏
+  
 int main()
 {
-	int a[] = { 1,3,4,7,8,6,10 };
-	AVLTree<int, int> a1;
-	for (auto e : a)
-	{
-		a1.insert({ e,e });
-	}
-	
-	a1.InOrder();
-	cout << a1.IsBalance() << endl;
+	Person p1;
+	Student s1;
+	p1.BuyTIcket();
+	s1.BuyTIcket();
 	return 0;
 }
